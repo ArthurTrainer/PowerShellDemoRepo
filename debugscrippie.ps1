68819417c27324b502cf0497cf806cb8dca9adfe
ArthurTrainer <arthurg@4knowledge.nl>
2025-10-08 15:18:43 +0200

    Add files via upload

---
+++ debugscrippie.ps1
@@ -0,0 +1,15 @@
+﻿#save your script with a nice name
+# add a breakpoint F9
+# continue next breakpoint F5
+#stop debugger Shift F5
+
+$getal=0
+while($getal -ne 7)
+{
+    $getal=$getal+1
+    Write-Host "het getal is:" $getal
+}
+
+
+#beter dan +1
+#$getal++
