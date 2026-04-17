0fab808129ed8f952374855bc1efc72e39843200
ArthurTrainer <arthurg@4knowledge.nl>
2026-04-17 11:42:10 +0200

    Add files via upload

---
+++ arthuropruimscript.ps1
@@ -0,0 +1,14 @@
+﻿#$folder = "$env:USERPROFILE\Downloads"
+$folder="C:\users\ArthurGoudswaard(4kn\downloadtest"
+$minutengewonefiles=1
+$minutenpdf=2
+#$days = 30
+
+
+Get-ChildItem $folder -File |
+Where-Object { ($_.LastWriteTime -lt (Get-Date).AddMinutes(-$minutengewonefiles)) -and -not ($_.FullName -like "*.pdf") } |
+Remove-Item -Force
+
+Get-ChildItem $folder -File |
+Where-Object { $_.LastWriteTime -lt (Get-Date).AddMinutes(-$minutenpdf) -and $_.FullName -like "*.pdf" } |
+Remove-Item -Force
